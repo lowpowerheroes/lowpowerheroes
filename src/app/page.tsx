0dd265f4b3335714e35f8353b9f94a55a522050e
fabripetrelli <petrellifabriziopaolo@gmail.com>
2024-10-31 01:37:18 +0100

    feat: add auth with clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <Image
-      src={"/assets/IMG_0902.png"}
-      height={3456}
-      width={5184}
-      alt="coming soon..."
-    />
-  );
+  return <div></div>;
 }
