0f45745d25b0582ca4cab247e2130c44a2a474fd
fabripetrelli <petrellifabriziopaolo@gmail.com>
2025-05-16 15:32:02 +0200

    chore: radix-ui themes

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -12,15 +12,14 @@ import { createQueryClient } from "./query-client";
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(async () => {
-  const headersList = await headers();
-  headersList.set("x-trpc-source", "rsc");
+const createContext = cache(() => {
+  const heads = new Headers(headers());
+  heads.set("x-trpc-source", "rsc");
 
   return createTRPCContext({
-    headers: headersList,
+    headers: heads,
   });
 });
-
 const getQueryClient = cache(createQueryClient);
 const caller = createCaller(createContext);
 
