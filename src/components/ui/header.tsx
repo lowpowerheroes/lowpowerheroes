2a10562df3a290214a4cf509ef1d0c3afcebc95a
fabripetrelli <petrellifabriziopaolo@gmail.com>
2025-05-16 12:57:15 +0200

    chore: upload your build button

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,21 +1,25 @@
 import Link from "next/link";
 import ThemeToggle from "./theme-toggle";
 import Search from "~/components/ui/search";
+import { Button } from "~/components/ui/button";
 import { Separator } from "~/components/ui/separator";
+
 const Header = () => {
   return (
     <div className="bg-white dark:bg-black flex w-full flex-col items-center">
       <header className="flex w-full justify-between px-60 py-5 align-middle">
         <div className="h-10">LowPowerHeroes</div>
         <div className="flex h-10 items-center justify-center space-x-2">
-          <Link
-            href={
-              "mailto:lowwpowerheroes@gmail.com?subject=Upload%20your%20build"
-            }
-            className="w-full"
-          >
-            Upload your build!
-          </Link>
+          <Button variant={"link"} asChild>
+            <Link
+              href={
+                "mailto:lowwpowerheroes@gmail.com?subject=Upload%20your%20build"
+              }
+              className="w-full"
+            >
+              Upload your build!
+            </Link>
+          </Button>
           <Search />
           <ThemeToggle />
         </div>
