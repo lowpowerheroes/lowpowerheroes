b55aa9e533c7cf21b1667388e391c2e67e639410
fabripetrelli <petrellifabriziopaolo@gmail.com>
2025-05-16 15:50:42 +0200

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "~/components/ui/header";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  void api.build.getLatest.prefetch();
+  await api.build.getLatest.prefetch();
 
   return (
     <HydrateClient>
