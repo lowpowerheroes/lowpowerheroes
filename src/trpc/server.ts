f841bbe024d700913e49d709eb1abfaa631c0112
fabripetrelli <petrellifabriziopaolo@gmail.com>
2025-05-16 14:54:40 +0200

    chore: remove error on trpc server

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -12,12 +12,12 @@ import { createQueryClient } from "./query-client";
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(() => {
-  const heads = new Headers(headers());
-  heads.set("x-trpc-source", "rsc");
+const createContext = cache(async () => {
+  const headersList = await headers();
+  headersList.set("x-trpc-source", "rsc");
 
   return createTRPCContext({
-    headers: heads,
+    headers: headersList,
   });
 });
 
